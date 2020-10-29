f91fa4dfc35ff95b28289cf67ec3119323b67184
Arushi2006 <72396009+Arushi2006@users.noreply.github.com>
2020-10-29 07:09:20 -0700

    Update ground.js

--- ground.js
+++ ground.js
@@ -1,10 +1,23 @@
 class Ground
   {
-    constructor()
+    constructor(x,y,width)
     {
         var options=
         {
             isStatic:true,
         }
+
+        this.ground=Bodies.rectangle(x,y,width,20,options);
+        this.width=width;
+        this.height=20;
+        World.add(world,this.ground);
+
+
+    }
+
+    display()
+    {
+      rectMode(CENTER);
+      rect(this.ground.position.x,this.ground.position.y,this.width,this.height);
     }
   }
