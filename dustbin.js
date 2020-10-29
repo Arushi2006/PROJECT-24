b64a9f76b81b3a3e4d3cd92a906fd87962cf6ade
Arushi2006 <72396009+Arushi2006@users.noreply.github.com>
2020-10-29 07:08:24 -0700

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -1,10 +1,24 @@
 class Dustbin
   {
-    constructor()
+    constructor(x,y,width,height)
     {
+  
         var options=
         {
             isStatic:true,
+
         }
-    }
+      
+      this.dustbinSprite=Bodies.rectangle(x,y,width,height,options);
+      this.width=width;
+      this.height=height;
+      this.image=loadImage("dustbingreen.png");
+      World.add(world,this.dustbinSprite);
+      
+      }
+
+      display()
+      {
+        image(this.image,this.dustbinSprite.position.x,this.dustbinSprite.position.y,this.width,this.height);
+      }
   }
